b104b7b8d081ceb603f27b6b6634bcc2df6c490b
Alexander Skachko <alexander.skachko@productmadness.com>
2021-05-04 12:08:49 +0300

    Fix codestyle

--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@ import ReactDOM from 'react-dom';
 
 import './styles/app.css';
 
-const App = () => {
-  return <div>Hello, World!</div>;
-}
+const App = () => (
+  <div>Hello, World!</div>
+);
 
 ReactDOM.render(<App />, document.getElementById('app'));
